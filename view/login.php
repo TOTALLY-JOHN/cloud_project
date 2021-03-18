0ed3196a030f83643013acdfd912c181469f63b0
suethui <j18028445@student.newinti.edu.my>
2021-03-18 21:27:44 +0800

    Updated LoginPage & Logo

--- view/login.php
+++ view/login.php
@@ -12,37 +12,65 @@
     <html>
 
     <head>
+        <title>Welcome to Tech Army</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Tech Army</title>
+
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">  
+        <link rel="stylesheet" type="text/css" href="../lib/styles/utilLogin.css">
+        <link rel="stylesheet" href="../lib/styles/login_style.css">
     </head>
-    <link rel="stylesheet" href="../lib/styles/login_style.css">
-    <style>
-        
-    </style>
     <body>
+    <div class="limiter">
+		<div class="loginContainer">
+			<div class="wrap-login100 p-l-85 p-r-85 p-t-40 p-b-55">
+                <div id="logoImageContainer">
+                    <img id="logoImage" src="../images/techlogo.png" alt="tech_army_logo" />
+                </div>
+				<form class="login100-form validate-form flex-sb flex-w" action = "" method="post" onsubmit=" return notify();">
+					<span class="loginTittle p-t-40 p-b-40">
+						Login
+					</span>
+				
+					<div class="wrap-input100 validate-input m-b-36">
+						<input type="text" class="input100" placeholder="Username " id="usernameInput" style="font-size: 16px;" pattern="^[A-Za-z0-9]{4,}$" title="Username must contain at least 4 characters." required="required">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</span>
+					</div>
+				
+					<div class="wrap-input100 validate-input m-b-12">
+						<input type="password" class="input100" placeholder="Password" id="passwordInput" style="font-size: 16px;" pattern="^.{8,}$" title="Password must contain at least 8 characters." required="required" >
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="w-full p-b-48" style="text-align: right;">
+						<div>
+							<a href="forgot_password.php" class="txt3" >
+								Forgot Password?
+							</a>
+						</div>
+					</div>
 
-        <div id="loginContainer">
-            <div id="logoImageContainer">
-                <img id="logoImage" src="../images/TechArmy_Logo.png" alt="tech_army_logo" />
-            </div>
-            <h1 id="loginHeader">Log In</h1>
-            <div id="loginBodyContainer">
-                <form method="post" autocomplete="off">
-                    <span class="loginLabel">Username</span><br />
-                    <input type="text" id="usernameInput" name="usernameInput" placeholder="Type your username" required/><br />
-                    <span class="loginLabel">Password</span><br />
-                    <input type="password" id="pwdInput" name="pwdInput" placeholder="Type your password" required/><br />
-                    <div id="forgotPwdLabel"><a id="forgotPwdLink" href="forgot_password.php">Forgot Password?</a></div><br />
-                    <input type="submit" class="loginBtn" value="LOGIN" /><br />
-                </form>
+					<div class="conatinerLoginButton">
+						<button class="logIn" style="color: white; ">
+							Login
+						</button>
+					</div>
 
-                <div id="signupLabel">
-                    <p class="loginLabel">Don&apos;t have an account?</p>
-                    <a id="signupLink" href="signup.php">Sign Up</a>
-                </div><br />
-            </div>
-
-        </div>
+				</form>
+                <div id="signupLabel" >
+                        <p class="txt2">Don&apos;t have an account?</p>
+                        <a href="signup.php" class="txt3">Sign Up
+							
+						</a>
+				</div>
+			</div>
+		</div>
+	</div>
 
         <footer>
         </footer>
