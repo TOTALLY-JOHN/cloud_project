28d90dad7007c0be936b2c521d094227867f344b
jihwan.jeong <wnaks5945@gmail.com>
2021-03-20 18:47:11 +0800

    signup error fixed

--- view/signup.php
+++ view/signup.php
@@ -15,7 +15,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Tech Army</title>
     </head>
-    <link rel="stylesheet" href="../lib/styles/login_style.css">
+    <link rel="stylesheet" href="../lib/styles/signup_style.css">
     <script type="text/javascript">
         function matchPassword(x, y){
             if (x.value == y.value){
@@ -34,22 +34,22 @@
     </style>
     <body>
 
-        <div id="loginContainer">
+        <div id="signupContainer">
             <div id="logoImageContainer">
-                <img id="logoImage" src="../images/TechArmy_Logo.png" alt="tech_army_logo" />
+                <img id="logoImage" src="../images/techlogo1.png" alt="tech_army_logo" style="width:50%" />
             </div>
-            <h1 id="loginHeader">Sign Up</h1>
-            <div id="loginBodyContainer">
-                <form method="postw" autocomplete="off">
-                    <span class="loginLabel">Email</span><br />
+            <h1 id="signupHeader">Sign Up</h1>
+            <div id="signupBodyContainer">
+                <form method="post" autocomplete="off">
+                    <span class="signupLabel">Email</span><br />
                     <input type="text" id="emailInput" name="emailInput" placeholder="Type your email" required/><br />
-                    <span class="loginLabel">Username</span><br />
+                    <span class="signupLabel">Username</span><br />
                     <input type="text" id="usernameInput" name="usernameInput" placeholder="Type your username" required/><br />
-                    <span class="loginLabel">Password</span><br />
+                    <span class="signupLabel">Password</span><br />
                     <input type="password" id="pwdInput1" name="pwdInput1" placeholder="Type your password" required/><br />
-                    <span class="loginLabel">Re-enter Password</span><br />
+                    <span class="signupLabel">Re-enter Password</span><br />
                     <input type="password" id="pwdInput2" name="pwdInput2" placeholder="Re-enter your password" required/><br />
-                    <input type="submit" class="loginBtn" value="SIGNUP" onclick="matchPassword(pwdInput1, pwdInput2)" /><br />
+                    <input type="submit" class="signupBtn" value="SIGNUP" onclick="matchPassword(pwdInput1, pwdInput2)" /><br />
                 </form>
             </div>
 
