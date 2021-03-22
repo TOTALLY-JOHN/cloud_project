f7a38592bbd261efd69dadae61140f418808c611
jihwan.jeong <wnaks5945@gmail.com>
2021-03-23 00:19:21 +0800

    Disable session for testing first

--- view/dashboard_disk.php
+++ view/dashboard_disk.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION['username'])) {
-    header('location: login.php');
-}
+// if (!isset($_SESSION['username'])) {
+//     header('location: login.php');
+// }
 ?>
 <DOCTYPE html>
     <html>
