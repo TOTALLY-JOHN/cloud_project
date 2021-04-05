d81e3318fe1461770ac2bf1be2fbeb8d9d5fd737
nostaxx <60691398+nostaxx@users.noreply.github.com>
2021-04-05 18:51:41 +0800

    Update error.php

--- view/error.php
+++ view/error.php
@@ -1,21 +1,31 @@
 <DOCTYPE html>
     <html>
-
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="../lib/styles/utilLogin.css">
+        <link rel="stylesheet" href="../lib/styles/login_style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <title>Tech Army</title>
     </head>
     <link rel="stylesheet" href="../lib/styles/login_style.css">
 
     <body>
-
-        <div id="dashboardContainer">
-            <h1>YOUR INPUT IS WRONG!</h1>
-            <?php echo $result; ?>
-
+        <div class="limiter">
+            <div class="loginContainer">
+                <div class="wrap-login100 p-l-85 p-r-85 p-t-40 p-b-55">
+                    <div id="logoImageContainer">
+                        <a href="../view/login.php"><img id="logoImage" src="../images/techlogo1.png" alt="tech_army_logo" style="width:50%" /></a>
+                        <h2 style="padding: 25px">(error Msg)</h2>
+                        <button style="" class="login" onclick="window.close()">Close</button>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
 
-        <footer>
-        </footer>
     </body>
 </html>
