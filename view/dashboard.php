da309cc204c2f6813d4465c428f2f50f72c4241d
jihwan.jeong <wnaks5945@gmail.com>
2021-07-02 02:25:08 +0800

    lang update

--- view/dashboard.php
+++ view/dashboard.php
@@ -337,7 +337,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <div class="col-lg-12 col-md-12">
                         <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title"> Dashboard Table</h4>
+                            <h4 class="card-title"> <?php echo $languages[$lang]['dashboard'];?></h4>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
